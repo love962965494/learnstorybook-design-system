13a013e77408ff9a56253b1377f572177b4de270
shenchao <shenchao20@jd.com>
2020-03-03 15:32:55 +0800

    添加文档支持

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { addDecorator } from '@storybook/react'
 import { GlobalStyle } from '../src/shared/global'
+import { withA11Y } from '@storybook/addon-a11y'
 
+addDecorator(withA11Y)
 addDecorator(story => (
   <>
     <GlobalStyle />
