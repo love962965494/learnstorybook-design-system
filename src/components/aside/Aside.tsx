42c65821358847af5750f3e94815f2b91df5b491
shenchao <shenchao20@jd.com>
2020-03-25 16:10:39 +0800

    fix(src/components/* src/examples): 修改组件代码格式bug；删除测试组件

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Layout, Menu } from 'antd'
 import PropTypes from 'prop-types'
 import './style/index.less'
@@ -10,31 +10,41 @@ interface AsideProps {
   title: string
 }
 
-export default function Aside(props: AsideProps) {
+const Aside: FC<AsideProps> = props => {
   const { title } = props
 
   return (
     <Sider className="neko-aside">
       <div className="logo">{title}</div>
       <Menu theme="dark" mode="inline">
-        <MenuItem key="1" title="首页">首页</MenuItem>
-        <MenuItem key="2" title="人员管理">人员管理</MenuItem>
+        <MenuItem key="1" title="首页">
+          首页
+        </MenuItem>
+        <MenuItem key="2" title="人员管理">
+          人员管理
+        </MenuItem>
         <SubMenu key="3" title="系统设置">
-          <MenuItem key="31" title="菜单设置">菜单设置</MenuItem>
-          <MenuItem key="32" title="权限设置">权限设置</MenuItem>
+          <MenuItem key="31" title="菜单设置">
+            菜单设置
+          </MenuItem>
+          <MenuItem key="32" title="权限设置">
+            权限设置
+          </MenuItem>
         </SubMenu>
       </Menu>
     </Sider>
   )
 }
 
+export default Aside
+
 Aside.propTypes = {
   /**
    * 项目标题
    */
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 }
 
 Aside.defaultProps = {
-  title: ''
+  title: '',
 }
